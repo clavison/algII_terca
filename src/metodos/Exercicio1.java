e7fdbb273b9a36185b0f09982cd24b063afadf55
clavison <clavison@gmail.com>
2024-04-23 20:19:11 -0300

    Enunciado exerc1 métodos

---
+++ src/metodos/Exercicio1.java
@@ -0,0 +1,16 @@
+package metodos;
+
+/*
+ Faça um programa que apresente o seguinte menu:
+1 - Cadastrar pessoa (nome, idade e sexo)
+2 - Buscar por nome
+3 - Média das idades dos homens
+4 - Média das idades das mulheres
+5 - Quantidade de mulheres com menos de 18 anos 
+6 - Sair
+
+Deve ser utilizado sub-rotinas.
+ */
+public class Exercicio1 {
+
+}
